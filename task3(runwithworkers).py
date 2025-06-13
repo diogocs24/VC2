f45111e0cb3bd2aec0650559e30d689b3314b97e
Diogo Silva <dcoutinhosilva03@gmail.com>
2025-06-13 02:39:49 +0100

    markdwon

--- task3(runwithworkers).py
+++ task3(runwithworkers).py
@@ -31,7 +31,7 @@ random.seed(42)
 
 data_aug = transforms.Compose([
     transforms.ToImage(),    
-    transforms.Resize((192, 192)),
+    transforms.Resize((256, 256)),
     #transforms.CenterCrop((224, 224)),
     #transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
@@ -41,7 +41,7 @@ data_aug = transforms.Compose([
 
 data_in = transforms.Compose([
     transforms.ToImage(),
-    transforms.Resize((192, 192)),
+    transforms.Resize((256, 256)),
     #transforms.CenterCrop((224, 224)),
     transforms.ToDtype(torch.float32, scale=True),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -109,6 +109,7 @@ class CocoBoardDataset(Dataset):
         # Generate mask
         mask = np.zeros((orig_height, orig_width), dtype=np.uint8)
         cv2.fillPoly(mask, [polygon], 1)  # Mask will have values 0 or 1
+        mask = cv2.resize(mask, (image.shape[-1], image.shape[-2]), interpolation=cv2.INTER_NEAREST)
 
         x_min = np.min(polygon[:, 0])
         y_min = np.min(polygon[:, 1])
